aa0427ec44a7bda35d9fe485393f2eb4d656266a
Bruno Fialho <brugerf@gmail.com>
2023-02-19 15:11:46 -0300

    feat: storing messages from users

--- scripts/run.ts
+++ scripts/run.ts
@@ -4,7 +4,21 @@ async function main() {
   const waveContractFactory = await ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract addy:", waveContract.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
+
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait();
+
+  const [_, randomPerson] = await ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  await waveTxn.wait();
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
